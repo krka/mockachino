36f290e67d7b85a5d48c28fec455b43fda61d86f
kristofer.karlsson <kristofer.karlsson@903b6af0-0dfe-11df-bbbe-ede73d0abfb9>
2010-02-11 22:14:35 +0000

    updated test

--- test/se/mockachino/matchers/matcher/AnyMatcherTest.java
+++ test/se/mockachino/matchers/matcher/AnyMatcherTest.java
@@ -9,7 +9,7 @@ public class AnyMatcherTest {
 	@Test
 	public void testSimple() {
 		Matcher<Integer> matcher = MatchersBase.mAny(Integer.class);
-		assertEquals("<mAny>", matcher.toString());
+		assertEquals("<Any>", matcher.toString());
 		assertEquals(true, matcher.matches(null));
 		assertEquals(true, matcher.matches(124));
 	}
