dd26492744b3255ae76f1c14c3e8930900ec6af3
kristofer.karlsson@gmail.com <kristofer.karlsson@gmail.com@903b6af0-0dfe-11df-bbbe-ede73d0abfb9>
2012-11-29 13:40:46 +0000

    Add option for disabling stack trace cleaner

--- src/main/java/se/mockachino/cleaner/StacktraceCleaner.java
+++ src/main/java/se/mockachino/cleaner/StacktraceCleaner.java
@@ -5,11 +5,14 @@ import java.util.Collection;
 import java.util.List;
 
 public class StacktraceCleaner {
+  public static final boolean ENABLED = false;
 
 	public static <T extends Throwable> T cleanError(T e) {
-		e.setStackTrace(convert(getCleanStacktrace(e)));
-		return e;
-	}
+    if (ENABLED) {
+      e.setStackTrace(convert(getCleanStacktrace(e)));
+    }
+    return e;
+  }
 
 	private static List<StackTraceElement> getCleanStacktrace(Throwable e) {
 		StackTraceElement[] stackTraceElements = e.getStackTrace();
