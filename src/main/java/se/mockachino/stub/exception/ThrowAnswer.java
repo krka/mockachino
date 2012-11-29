80fb972b0442624f55a2b5a3d2da52af74e57175
kristofer.karlsson@gmail.com <kristofer.karlsson@gmail.com@903b6af0-0dfe-11df-bbbe-ede73d0abfb9>
2012-11-29 14:08:00 +0000

    Fix bug where some exceptions can't be fake-created.

--- src/main/java/se/mockachino/stub/exception/ThrowAnswer.java
+++ src/main/java/se/mockachino/stub/exception/ThrowAnswer.java
@@ -1,6 +1,5 @@
 package se.mockachino.stub.exception;
 
-import se.mockachino.CallHandler;
 import se.mockachino.MethodCall;
 import se.mockachino.VerifyableCallHandler;
 import se.mockachino.exceptions.UsageError;
@@ -10,69 +9,101 @@ import java.lang.reflect.Constructor;
 
 public class ThrowAnswer implements VerifyableCallHandler {
 
-	private final Constructor<Throwable> constructor;
-	private final Object[] params;
-    private final Class<? extends Throwable> clazz;
+  private final Constructor<Throwable> constructor;
+  private final Class<? extends Throwable> clazz;
+  private final Throwable throwable;
+  private final Object[] initArgs;
 
-    public ThrowAnswer(Throwable e) {
-        this.clazz = e.getClass();
+  public ThrowAnswer(Throwable e) {
+    this.clazz = e.getClass();
 
-		Throwable cause = e.getCause();
-		String message = e.getMessage();
-
-		int bestScore = -1;
-		Constructor<Throwable> bestConstructor = null;
-		Object[] bestParams = null;
-		for (Constructor<?> constructor : e.getClass().getConstructors()) {
-			Class<?>[] paramTypes = constructor.getParameterTypes();
-			int n = paramTypes.length;
-			Object[] objects = new Object[n];
-			int foundString = 0;
-			int foundCause = 0;
-			for (int i = 0; i < n; i++) {
-				Class<?> paramType = paramTypes[i];
-				if (paramType == String.class) {
-					objects[i] = message;
-					foundString = 1;
-				} else if (cause != null && paramType.isAssignableFrom(cause.getClass())) {
-					objects[i] = cause;
-					foundCause = 1;
-				}
-
-			}
-			int score = foundString + foundCause;
-			if (score > bestScore) {
-				constructor.setAccessible(true);
-				bestScore = score;
-				bestConstructor = (Constructor<Throwable>) constructor;
-				bestParams = objects;
-			}
-		}
-		if (bestConstructor == null) {
-			throw new UsageError("Could not find a suitable constructor for " + e.getClass());
-		}
-		constructor = bestConstructor;
-		params = bestParams;
-	}
-
-	@Override
-	public Object invoke(Object obj, MethodCall call) throws Throwable {
-		throw constructor.newInstance(params);
-	}
-
-    @Override
-    public void verify(MockachinoMethod method) {
-        if (RuntimeException.class.isAssignableFrom(clazz) ||
-                Error.class.isAssignableFrom(clazz)) {
-            return;
+    Constructor<Throwable> bestConstructor = findBestConstructor(e.getClass());
+    if (bestConstructor != null) {
+      throwable = null;
+      constructor = bestConstructor;
+      Class<?>[] parameterTypes = constructor.getParameterTypes();
+      initArgs = new Object[parameterTypes.length];
+      for (int i = 0; i < initArgs.length; i++) {
+        Class<?> paramClass = parameterTypes[i];
+        if (paramClass == String.class) {
+          initArgs[i] = e.getMessage();
+        } else if (paramClass == Throwable.class) {
+          initArgs[i] = e.getCause();
+        } else {
+          throw new RuntimeException();
         }
-
-        Class<?>[] validExceptions = method.getMethod().getExceptionTypes();
-        for (Class<?> validException : validExceptions) {
-            if (validException.isAssignableFrom(clazz)) {
-                return;
-            }
-        }
-        throw new UsageError(method + " may not throw " + clazz);
+      }
+    } else {
+      throwable = e;
+      constructor = null;
+      initArgs = null;
     }
+  }
+
+  private Constructor<Throwable> findBestConstructor(Class<?> clazz) {
+    Constructor<Throwable> bestConstructor = null;
+    int bestScore = 0;
+
+    for (Constructor<?> constructor : clazz.getConstructors()) {
+      Class<?>[] paramTypes = constructor.getParameterTypes();
+      int score = getScore(paramTypes);
+      if (score > bestScore) {
+        bestScore = score;
+        bestConstructor = (Constructor<Throwable>) constructor;
+      }
+    }
+    return bestConstructor;
+  }
+
+  private int getScore(Class<?>[] paramTypes) {
+    if (equals(paramTypes, String.class, Throwable.class)) {
+      return 4;
+    }
+    if (equals(paramTypes, Throwable.class, String.class)) {
+      return 3;
+    }
+    if (equals(paramTypes, Throwable.class)) {
+      return 2;
+    }
+    if (equals(paramTypes, String.class)) {
+      return 1;
+    }
+    return 0;
+  }
+
+  private boolean equals(Class<?>[] paramTypes, Class<?>... b) {
+    if (paramTypes.length != b.length) {
+      return false;
+    }
+    for (int i = 0; i < paramTypes.length; i++) {
+      if (!paramTypes[i].equals(b[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public Object invoke(Object obj, MethodCall call) throws Throwable {
+    if (throwable != null) {
+      throw throwable;
+    }
+    throw constructor.newInstance(initArgs);
+  }
+
+  @Override
+  public void verify(MockachinoMethod method) {
+    if (RuntimeException.class.isAssignableFrom(clazz) ||
+            Error.class.isAssignableFrom(clazz)) {
+      return;
+    }
+
+    Class<?>[] validExceptions = method.getMethod().getExceptionTypes();
+    for (Class<?> validException : validExceptions) {
+      if (validException.isAssignableFrom(clazz)) {
+        return;
+      }
+    }
+    throw new UsageError(method + " may not throw " + clazz);
+  }
 }
--- test/blackbox/se/mockachino/ThrowsTest.java
+++ test/blackbox/se/mockachino/ThrowsTest.java
@@ -10,90 +10,109 @@ import java.net.SocketAddress;
 import java.util.List;
 
 import static junit.framework.Assert.*;
+import static se.mockachino.Mockachino.assertThrows;
+import static se.mockachino.Mockachino.mock;
+import static se.mockachino.Mockachino.stubThrow;
 
 public class ThrowsTest {
-	@Test
-	public void testCorrectStacktrace() {
-		List mock = Mockachino.mock(List.class);
-		Mockachino.when(mock.size()).thenThrow(new RuntimeException());
+  @Test
+  public void testCorrectStacktrace() {
+    List mock = Mockachino.mock(List.class);
+    Mockachino.when(mock.size()).thenThrow(new RuntimeException());
 
-		for (int i = 0; i < 10; i++) {
-			// Do some work, to make sure expected will have a different line than the one above.
-		}
+    for (int i = 0; i < 10; i++) {
+      // Do some work, to make sure expected will have a different line than the one above.
+    }
 
-		RuntimeException expected = new RuntimeException();
-		try {
-			mock.size();
-		} catch (Exception e) {
-			StackTraceElement[] exp = expected.getStackTrace();
-			StackTraceElement[] act = e.getStackTrace();
+    RuntimeException expected = new RuntimeException();
+    try {
+      mock.size();
+    } catch (Exception e) {
+      StackTraceElement[] exp = expected.getStackTrace();
+      StackTraceElement[] act = e.getStackTrace();
 
-			assertEquals(exp[0].getClassName(), act[0].getClassName());
-			assertEquals(exp[0].getLineNumber() + 2, act[0].getLineNumber());
-		}
+      assertEquals(exp[0].getClassName(), act[0].getClassName());
+      assertEquals(exp[0].getLineNumber() + 2, act[0].getLineNumber());
+    }
 
-	}
+  }
 
-	@Test
-	public void testNewExceptions() {
-		List mock = Mockachino.mock(List.class);
-		IOException cause = new IOException();
-		RuntimeException exception = new RuntimeException("Hello world", cause);
-		Exception exception2 = null;
-		Mockachino.when(mock.size()).thenThrow(exception);
-		try {
-			mock.size();
-			fail();
-		} catch (Exception e) {
-			assertEquals(cause, e.getCause());
-			assertEquals("Hello world", e.getMessage());
-			assertFalse(exception == e);
-			exception2 = e;
-		}
-		try {
-			mock.size();
-			fail();
-		} catch (Exception e) {
-			assertFalse(e == exception2);
-		}
-	}
+  @Test
+  public void testNewExceptions() {
+    List mock = Mockachino.mock(List.class);
+    IOException cause = new IOException();
+    RuntimeException exception = new RuntimeException("Hello world", cause);
+    Exception exception2 = null;
+    Mockachino.when(mock.size()).thenThrow(exception);
+    try {
+      mock.size();
+      fail();
+    } catch (Exception e) {
+      assertEquals(cause, e.getCause());
+      assertEquals("Hello world", e.getMessage());
+      assertFalse(exception == e);
+      exception2 = e;
+    }
+    try {
+      mock.size();
+      fail();
+    } catch (Exception e) {
+      assertFalse(e == exception2);
+    }
+  }
 
-	@Test(expected = UsageError.class)
-	public void validThrow() {
-		List mock = Mockachino.mock(List.class);
-		Mockachino.when(mock.size()).thenThrow(new IOException());
-	}
+  @Test(expected = UsageError.class)
+  public void validThrow() {
+    List mock = Mockachino.mock(List.class);
+    Mockachino.when(mock.size()).thenThrow(new IOException());
+  }
 
-	@Test
-	public void testCheckedException() throws IOException {
-		Socket mock = Mockachino.mock(Socket.class);
-		Mockachino.stubThrow(new IOException()).on(mock).connect(Matchers.any(SocketAddress.class));
-	}
+  @Test
+  public void testCheckedException() throws IOException {
+    Socket mock = Mockachino.mock(Socket.class);
+    Mockachino.stubThrow(new IOException()).on(mock).connect(Matchers.any(SocketAddress.class));
+  }
 
-	@Test(expected = UsageError.class)
-	public void testCheckedExceptionFail() throws IOException {
-		Socket mock = Mockachino.mock(Socket.class);
-		Mockachino.stubThrow(new ClassNotFoundException()).on(mock).connect(Matchers.any(SocketAddress.class));
-	}
+  @Test(expected = UsageError.class)
+  public void testCheckedExceptionFail() throws IOException {
+    Socket mock = Mockachino.mock(Socket.class);
+    Mockachino.stubThrow(new ClassNotFoundException()).on(mock).connect(Matchers.any(SocketAddress.class));
+  }
 
-	@Test
-	public void testExceptionClasses() {
-		helper(new RuntimeException("Hello world", new IOException()));
-		helper(new IllegalArgumentException("Hello world", new IOException()));
-		helper(new Error("Hello world", new IOException()));
-		helper(new StackOverflowError("Hello world"));
-		helper(new UsageError("Hello world"));
-	}
+  @Test
+  public void testExceptionClasses() {
+    helper(new RuntimeException("Hello world", new IOException()));
+    helper(new IllegalArgumentException("Hello world", new IOException()));
+    helper(new Error("Hello world", new IOException()));
+    helper(new StackOverflowError("Hello world"));
+    helper(new UsageError("Hello world"));
+  }
 
-	private void helper(Throwable input) {
-		List mock = Mockachino.mock(List.class);
-		Mockachino.when(mock.size()).thenThrow(input);
-		try {
-			mock.size();
-			fail();
-		} catch (Throwable e) {
-			assertEquals(input.getCause(), e.getCause());
-			assertEquals(input.getMessage(), e.getMessage());
-		}
-	}
+  private void helper(Throwable input) {
+    List mock = Mockachino.mock(List.class);
+    Mockachino.when(mock.size()).thenThrow(input);
+    try {
+      mock.size();
+      fail();
+    } catch (Throwable e) {
+      assertEquals(input.getCause(), e.getCause());
+      assertEquals(input.getMessage(), e.getMessage());
+    }
+  }
+
+  @Test
+  public void testNonStandardConstructor() throws Exception {
+    TestClass testClass = mock(TestClass.class);
+    stubThrow(new TestException(100)).on(testClass).doIt();
+    assertThrows(TestException.class).on(testClass).doIt();
+  }
+
+  public interface TestClass {
+    void doIt() throws TestException;
+  }
+
+  public class TestException extends Exception {
+    public TestException(int val) {
+    }
+  }
 }
